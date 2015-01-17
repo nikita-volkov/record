c52222c472eb832b7a7b895dc714a3325085e0da
Nikita Volkov <nikita.y.volkov@mail.ru>
2015-01-17 03:23:29 +0300

    Tuple instances

--- library/Record/Field.hs
+++ library/Record/Field.hs
@@ -5,13 +5,25 @@ import GHC.TypeLits
 import Record.Lens (Lens)
 
 
-newtype Field (n :: Symbol) a = Field a
+class Owner (n :: Symbol) v a | n a -> v where
+  setField :: Proxy n -> v -> a -> a
+  getField :: Proxy n -> a -> v
 
-class Owner n a where
-  type Value n a
-  setField :: Proxy n -> Value n a -> a -> a
-  getField :: Proxy n -> a -> Value n a
-
-lens :: Owner n a => Proxy n -> Lens a (Value n a)
+lens :: Owner n v a => Proxy n -> Lens a v
 lens n =
   \f a -> fmap (\v -> setField n v a) (f (getField n a))
+
+
+newtype Field (n :: Symbol) v = Field v
+
+instance Owner n v (Field n v) where
+  setField _ v _ = Field v
+  getField _ (Field v) = v
+
+instance Owner n1 v1 (Field n1 v1, Field n2 v2) where
+  setField _ v (f1, f2) = (Field v, f2)
+  getField _ (Field v, _) = v
+
+instance Owner n2 v2 (Field n1 v1, Field n2 v2) where
+  setField _ v (f1, f2) = (f1, Field v)
+  getField _ (_, Field v) = v
