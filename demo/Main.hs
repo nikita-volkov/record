2537be7799312da8fe23b1faf9efe3fc0820129c
Nikita Volkov <nikita.y.volkov@mail.ru>
2015-01-17 23:17:45 +0300

    Implement basic lens combinators

--- demo/Main.hs
+++ demo/Main.hs
@@ -12,6 +12,19 @@ main =
 type User = 
   [record| {name :: String, birthday :: {year :: Int, month :: Int, day :: Int}} |]
 
+getUserBirthdayYear :: User -> Int
+getUserBirthdayYear =
+  view [lens|birthday.year|]
+
+setUserBirthdayYear :: Int -> User -> User
+setUserBirthdayYear =
+  set [lens|birthday.year|]
+
 userBirthdayYearLens :: Lens User Int
 userBirthdayYearLens =
-  [lens| birthday.year |]
+  [lens|birthday.year|]
+
+userBirthdayYearLens' :: Lens User Int
+userBirthdayYearLens' =
+  [lens|birthday|] . [lens|year|]
+
--- library/Record/Lens.hs
+++ library/Record/Lens.hs
@@ -4,10 +4,20 @@
 module Record.Lens where
 
 import BasePrelude
+import Data.Functor.Identity
 
 
 type Lens s a = 
   forall f. Functor f => (a -> f a) -> (s -> f s)
 
+view :: Lens s a -> s -> a
+view l =
+  fst . l (\a -> (a, a))
 
+set :: Lens s a -> a -> s -> s
+set l a =
+  runIdentity . l (Identity . const a)
 
+over :: Lens s a -> (a -> a) -> s -> s
+over l f =
+  runIdentity . l (Identity . f)
