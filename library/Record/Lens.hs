9868a11e9cb28b65fe9a4840e5c9dc770129b56b
Nikita Volkov <nikita.y.volkov@mail.ru>
2015-01-20 08:03:50 +0300

    Extend lens

--- library/Record/Lens.hs
+++ library/Record/Lens.hs
@@ -13,26 +13,26 @@ import Data.Functor.Identity
 -- |
 -- A reference from a datastructure @s@ to some part @a@,
 -- which can be used to manipulate that particular part.
-type Lens s a = 
-  forall f. Functor f => (a -> f a) -> (s -> f s)
+type Lens s s' a a' = 
+  forall f. Functor f => (a -> f a') -> (s -> f s')
 
 -- |
 -- Given a lens to a subpart and a datastructure,
 -- produce the value of the referred subpart.
-view :: Lens s a -> s -> a
+view :: Lens s s' a a' -> s -> a
 view l =
   getConst . l Const
 
 -- |
 -- Given a lens to a subpart, a new value for it and a datastructure,
 -- produce an updated datastructure.
-set :: Lens s a -> a -> s -> s
+set :: Lens s s' a a' -> a' -> s -> s'
 set l a =
   runIdentity . l (Identity . const a)
 
 -- |
 -- Given a lens to a subpart, and an update function for it,
 -- produce a function, which updates the datastructure.
-over :: Lens s a -> (a -> a) -> s -> s
+over :: Lens s s' a a' -> (a -> a') -> s -> s'
 over l f =
   runIdentity . l (Identity . f)
